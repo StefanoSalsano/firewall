aa4d9781c2590338d33c613c2bb000d52b80ea75
Stefano Salsano <stefano.salsano@uniroma2.it>
2024-03-26 18:28:12 +0100

    added blacklist in print_and_accept

--- print-accept.py
+++ print-accept.py
@@ -1,6 +1,14 @@
 from netfilterqueue import NetfilterQueue
 from scapy.all import *
 
+sni_black_list = [ 
+'www.uniroma2.it',
+'esamionline.uniroma2.it',
+'www.shorturl.at',
+'play.google.com'
+] 
+ 
+
 def print_and_accept(pkt):
     load_layer("tls")
     #print(pkt)
@@ -12,13 +20,23 @@ def print_and_accept(pkt):
     if sca.haslayer('TLS') and sca['TLS'].type == 22 and sca['TLS'].msg[0].msgtype == 1:
         
         msg = sca['IP'].src +' ==> ' +sca['IP'].dst +' : '+ (sca['TLS']['TLS_Ext_ServerName'].servernames[0].servername).decode("utf-8")
-        print(msg)
-        
+        #print(msg)
+        sni = (sca['TLS']['TLS_Ext_ServerName'].servernames[0].servername).decode("utf-8")
+        if (sni in sni_black_list) :
+            pkt.drop() 
+            print('DROP: '+sni)
+        else :
+            print('ACCEPT: '+sni)
+            pkt.accept()    
+                    
+    else :
             
     #except:
     #    pass
     
-    pkt.accept()
+        pkt.accept()
+
+
 
 nfqueue = NetfilterQueue()
 nfqueue.bind(1, print_and_accept)
